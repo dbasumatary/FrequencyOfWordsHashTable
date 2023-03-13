8dd5cfd1ac0c91b7d68936f4f4409a56fc9f3f70
dbasumatary <basudhondip@gmail.com>
2023-03-13 16:52:24 +0530

    Added: Program to find frequency of words in a large paragraph phrase

--- src/frequencyofwords/FrequencyOfWordsMain.java
+++ src/frequencyofwords/FrequencyOfWordsMain.java
@@ -3,7 +3,7 @@ package frequencyofwords;
 public class FrequencyOfWordsMain {
     public static void main(String[] args) {
         FrequencyOfWords words = new FrequencyOfWords();
-        words.findFrequency("To be or not to be");
+        words.findFrequency("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations");
     }
 }
 
